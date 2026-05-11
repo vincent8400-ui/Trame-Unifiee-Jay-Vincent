2e446edc37a5780b18ef8bcc66dc3f2b3affe6b0
Vincent Jay <picosnoop@gmail.com>
2026-05-11 19:40:02 +0200

    Update test_universel.py

--- test_universel.py
+++ test_universel.py
@@ -1,13 +1,13 @@
 c = 299792458
-maille_or = 1.573647
-cycle_gvj = 244
+maille_or = 1.6266666667
+cycle_gvj = 162.6
 
 print("--- TEST DE RÉSONANCE LUMIÈRE ---")
 rapport = c / maille_or
 residu = rapport % cycle_gvj
 
 print(f"Rapport Lumière/Maille : {rapport:.2f}")
-print(f"Résidu sur cycle 244   : {residu:.6f}")
+print(f"Résidu sur cycle 162.6   : {residu:.6f}")
 
 # Test de l'harmonique
 harmonique = rapport / cycle_gvj
